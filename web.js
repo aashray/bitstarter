6f28944efd33d3b74681b9d47e39d5d114f3bbff
Aashray <aashrayarora@gmail.com>
2013-07-11 07:12:31 +0000

    Readfile 2

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  // response.send('Hello World 2!');
- var contents = fs.readFile();
+ var contents = fs.readFile("./index.html");
  response.send(contents);
 });
 
