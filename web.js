6f561b9a16aca22619fbd32ad9352f138db70a11
Aashray <aashrayarora@gmail.com>
2013-07-11 07:10:23 +0000

    Trying readfile 1

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ // response.send('Hello World 2!');
+ var contents = fs.readFile();
+ response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
